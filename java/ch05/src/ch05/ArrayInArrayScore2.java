648fef563d192b4ddb74278b033167fb94d315c8
zzzless <beng0516@naver.com>
2021-03-31 11:21:46 +0900

    Use Scanner on Method
    메소드에서 Scanner를 사용하려면 전역변수로 사용해야한다.

--- java/ch05/src/ch05/ArrayInArrayScore2.java
+++ java/ch05/src/ch05/ArrayInArrayScore2.java
@@ -1,26 +1,47 @@
+
 package ch05;
 
 import java.util.Scanner;
 
 public class ArrayInArrayScore2 {
+	
+	public static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
+		String[] subject = {"국어","영어","수학"};
 		// 학생수 입력 받기
+		int students = inputStudents();
+		int[][] scores = new int[students][3];
+		// 점수 입력
+		inputScores(scores,students,subject);
+		// 학생당 평균
+		showAvgStudent(scores,students,subject);
+		// 과목당 평균
+		showAvgSubject(scores,students,subject);
+		scan.close();
+	}//main
+	
+	//학생수 입력
+	static int inputStudents() {
 		System.out.print("학생수>");
 		int students = scan.nextInt();
-		int[][] scores = new int[students][3];
-		String[] subject = {"국어","영어","수학"};
-		
-		// 점수 입력
+		return students;
+	}
+	
+	// 점수 입력
+	static void inputScores(int[][] scores, int students, String[] subject) {
 		for(int i=0;i<students;++i) {
 			for(int j=0;j<subject.length;++j) {
 				System.out.print((i+1)+"번째 학생, "+subject[j]+"> ");
-				scores[i][j] = scan.nextInt();
+				//System.out.println(scores[i][j]);
+				int a = scan.nextInt();
+				scores[i][j] = a;
 			}
 		}
-		
-		// 학생당 평균
+	}
+	
+	//각 학생의 평균
+	static void showAvgStudent(int[][] scores, int students, String[] subject) {
 		int sum = 0;
 		for(int i=0;i<students;++i) {
 			for(int j=0;j<subject.length;++j) {
@@ -29,8 +50,9 @@ public class ArrayInArrayScore2 {
 			System.out.println((i+1)+"번째 학생의 평균 점수: "+((double)sum/subject.length));
 			sum=0;
 		}
-		
-		// 과목당 평균
+	}
+	//과목당 평균
+	static void showAvgSubject(int[][] scores, int students, String[] subject) {
 		int total = 0;
 		double totalAvg = 0;
 		for(int j=0; j<subject.length; ++j) {
@@ -41,6 +63,6 @@ public class ArrayInArrayScore2 {
 			System.out.println(subject[j]+" 평균 : "+totalAvg);
 			total = 0;
 		}
-		scan.close();
 	}
 }
+
