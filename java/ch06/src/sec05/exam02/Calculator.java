3922dcf5a41eba7717da1ba38adb8509b84960bb
zzzless <beng0516@naver.com>
2021-04-02 16:38:00 +0900

    Static 정적변수, 정적메소드

---
+++ java/ch06/src/sec05/exam02/Calculator.java
@@ -0,0 +1,11 @@
+package sec05.exam02;
+
+public class Calculator {
+	static final double MATH_PI = 3.14;
+	static double getCircum(int r) {
+		return 2 * MATH_PI * r;
+	}
+	static double getArea(int r) {
+		return MATH_PI * r * r;
+	}
+}
