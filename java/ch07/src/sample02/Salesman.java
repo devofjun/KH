1eb74ad6730143a1fe810f052735fca4d7fb67df
zzzless <beng0516@naver.com>
2021-04-07 08:59:12 +0900

    testing

--- java/ch07/src/sample02/Salesman.java
+++ java/ch07/src/sample02/Salesman.java
@@ -1,6 +1,7 @@
 package sample02;
 
 public class Salesman extends Worker{
+	
 	public void work() {
 		System.out.println("판매합니다.");
 	}
@@ -8,3 +9,4 @@ public class Salesman extends Worker{
 		System.out.println("고객을 관리합니다.");
 	}
 }
+
--- java/ch07/src/sample02/Worker.java
+++ java/ch07/src/sample02/Worker.java
@@ -1,6 +1,7 @@
 package sample02;
 
 public class Worker {
+	Worker() { super(); }
 	public void work() {
 		System.out.println("일합니다.");
 	}
