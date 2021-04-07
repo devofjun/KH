c348b67ac2fe1cc60be043056dc595ada2368cbf
zzzless <beng0516@naver.com>
2021-04-07 14:10:03 +0900

    GUI 프로그래밍 시작

--- java/swing01/src/ex01/MyApp.java
+++ java/swing01/src/ex01/MyApp.java
@@ -1,5 +1,17 @@
 package ex01;
 
-public class MyApp {
+import javax.swing.JFrame;
 
+public class MyApp {
+	public static void main(String[] args) {
+		// 윈도우 생성
+		JFrame f = new JFrame();
+		// 창의 제목 설정
+		f.setTitle("첫번째 창");
+		// 창의 크기 설정
+		f.setSize(500, 300);
+		// 창을 화면에 보이는 상태로 설정
+		f.setVisible(true);
+		// 종료가 되게 하기
+	}
 }
