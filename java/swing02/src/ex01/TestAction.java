006b20bc47d73f19cf032e87485f0c00303b40ad
zzzless <beng0516@naver.com>
2021-04-08 23:33:09 +0900

    리스너연습

---
+++ java/swing02/src/ex01/TestAction.java
@@ -0,0 +1,38 @@
+package ex01;
+
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class TestAction extends JFrame{
+	Container ctn = getContentPane();
+	JButton btn = new JButton("");
+	ActionLsn listener = new ActionLsn();
+	
+	public TestAction() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("test");
+		btn.addActionListener(listener);
+		ctn.add(btn);
+		
+		setSize(500,500);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new TestAction();
+	}
+}
+
+
+class ActionLsn implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("할 수 있다.");
+	}
+	
+}
