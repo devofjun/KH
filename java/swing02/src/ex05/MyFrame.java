7265bff1ddebc1b27757f6505581c3c9c7ab0d83
zzzless <beng0516@naver.com>
2021-04-09 15:07:23 +0900

    리스너 삭제

--- java/swing02/src/ex05/MyFrame.java
+++ java/swing02/src/ex05/MyFrame.java
@@ -8,20 +8,19 @@ import javax.swing.JLabel;
 @SuppressWarnings("serial")
 public class MyFrame extends JFrame{
 	Container c = getContentPane();
-	MyMouseAdapter lsn = new MyMouseAdapter(); 
 	JLabel myLabel = new JLabel("Java");
 	
 	public MyFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("액션리스너");
-		lsn.setLblTarget(myLabel);
-		c.addMouseMotionListener(lsn); // 해당 리스너에 맞는 메소드를 호출해야한다.
 		c.setLayout(null);
 		myLabel.setBounds(10, 10, 50, 30);
 		c.add(myLabel);
+		
 		setSize(500, 500);
 		setVisible(true);
 	}
+	
 	
 	public static void main(String[] args) {
 		new MyFrame();
