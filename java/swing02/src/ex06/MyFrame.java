b1d54433b13475155b26de4b63636f46785d3e93
zzzless <beng0516@naver.com>
2021-04-09 15:50:22 +0900

    키보드로 라벨 이동시키기

--- java/swing02/src/ex06/MyFrame.java
+++ java/swing02/src/ex06/MyFrame.java
@@ -17,6 +17,7 @@ public class MyFrame extends JFrame{
 		c.setLayout(null);
 		myLabel.setBounds(10, 10, 50, 30);
 		c.add(myLabel);
+		lsn.setlbl(myLabel);
 		c.addKeyListener(lsn);
 		setSize(500, 500);
 		setVisible(true);
--- java/swing02/src/ex06/MyKeyAdapter.java
+++ java/swing02/src/ex06/MyKeyAdapter.java
@@ -3,24 +3,33 @@ package ex06;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class MyKeyAdapter extends KeyAdapter{
+import javax.swing.JLabel;
 
+public class MyKeyAdapter extends KeyAdapter{
+	private JLabel lbl = new JLabel();
+	public void setlbl(JLabel lbl) {
+		this.lbl = lbl;
+	}
+	
 	@Override
 	public void keyPressed(KeyEvent e) {
 		//System.out.println(e);
-		int k = e.getKeyCode();
-		switch(k) {
+		switch(e.getKeyCode()) {
 		case KeyEvent.VK_LEFT:
-			System.out.println("좌");
+			//System.out.println("좌");
+			lbl.setLocation(lbl.getX()-10, lbl.getY());
 			break;
 		case KeyEvent.VK_UP:
-			System.out.println("상");
+			//System.out.println("상");
+			lbl.setLocation(lbl.getX(), lbl.getY()-10);
 			break;
 		case KeyEvent.VK_RIGHT:
-			System.out.println("우");
+			//System.out.println("우");
+			lbl.setLocation(lbl.getX()+10, lbl.getY());
 			break;
 		case KeyEvent.VK_DOWN:
-			System.out.println("하");
+			//System.out.println("하");
+			lbl.setLocation(lbl.getX(), lbl.getY()+10);
 			break;
 		}
 	}
