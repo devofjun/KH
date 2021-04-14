bf508788772ce4b092d97b7751e639e8656e8056
zzzless <beng0516@naver.com>
2021-04-14 14:24:02 +0900

    수정

--- javaexam/exam01/src/exam01/Test1.java
+++ javaexam/exam01/src/exam01/Test1.java
@@ -15,7 +15,7 @@ public class Test1 {
 		//Á¤·Ä
 		int min = 99999999;
 		for(int i=0; i<arr.length-1; i++) {
-			for(int j=1; j<arr.length; j++) {
+			for(int j=i+1; j<arr.length; j++) {
 				if(arr[i]<arr[j]) {
 					int tmp = arr[i];
 					arr[i] = arr[j];
