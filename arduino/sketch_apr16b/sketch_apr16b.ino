2ee5a81c79f0e75c949a2f749a4ca0e62c4d694b
zzzless <beng0516@naver.com>
2021-04-16 10:16:37 +0900

    변동저항

---
+++ arduino/sketch_apr16b/sketch_apr16b.ino
@@ -0,0 +1,29 @@
+void setup() {
+  pinMode(A0,INPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(analogRead(A0));
+  int a = analogRead(A0);
+  if(a > 900) {
+    digitalWrite(11,HIGH);
+    digitalWrite(10,HIGH);
+    digitalWrite(9,HIGH);
+  } else if(a>600) {
+    digitalWrite(11,LOW);
+    digitalWrite(10,HIGH);
+    digitalWrite(9,HIGH);
+  } else if(a>300) {
+    digitalWrite(11,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(9,HIGH);
+  } else {
+    digitalWrite(11,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(9,LOW);
+  }
+}
