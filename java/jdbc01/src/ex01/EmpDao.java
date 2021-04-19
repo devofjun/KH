c866c253e7350e28784302301ce99ff3ccaa502c
zzzless <beng0516@naver.com>
2021-04-19 16:06:42 +0900

    DB값 가져와서 출력

--- java/jdbc01/src/ex01/EmpDao.java
+++ java/jdbc01/src/ex01/EmpDao.java
@@ -2,6 +2,8 @@ package ex01;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class EmpDao { // 데이터베이스 접근 객체 - DAO(Data Access Object)
 	private static EmpDao instance;
@@ -18,13 +20,43 @@ public class EmpDao { // 데이터베이스 접근 객체 - DAO(Data Access Obje
 	private final String ID = "user01";
 	private final String PW = "1234";
 	
-	public Connection getConnection() {
+	private Connection getConnection() {
 		try {
 			Class.forName(DRIVER_NAME); // SQL Command Line 띄우는거라고 생각하면됨
 			Connection conn = DriverManager.getConnection(URL, ID, PW); // SQL> conn user01/1234
 			System.out.println("conn:"+conn); // 연결이 잘 됐는지 확인
+			return conn;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public EmpVo getEmpList() {
+		try {
+			Connection conn = getConnection();
+			// SQL 문장 타이핑
+			String sql = "select empno, ename, job, sal"
+					+ "   from emp" // 공백이 있어야 from과 select문과 구분이 된다.
+					+ "   order by empno asc";  // 마찬가지로 공백이 있어야 한다.
+			PreparedStatement pstmt = conn.prepareStatement(sql); // SQL 문장 전달(타이핑)
+			ResultSet rs = pstmt.executeQuery(); // Enter
+			boolean b = rs.next(); // 더 읽어올 데이터가 있다면 true
+			if(b == true) {
+				int empno = rs.getInt("empno");
+				String ename = rs.getString("ename");
+				String job = rs.getString("job");
+				int sal = rs.getInt("sal");
+				
+				EmpVo empVo = new EmpVo();
+				empVo.setEmpno(empno);
+				empVo.setEname(ename);
+				empVo.setJob(job);
+				empVo.setSal(sal);
+				System.out.println(empVo.toString());
+				return empVo;
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
--- java/jdbc01/src/ex01/EmpFrame.java
+++ java/jdbc01/src/ex01/EmpFrame.java
@@ -3,19 +3,30 @@ package ex01;
 import java.awt.Container;
 
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 @SuppressWarnings("serial")
 public class EmpFrame extends JFrame{
 	EmpDao empDao = EmpDao.getInstance();
 	Container c = getContentPane();
-	
+	JTextArea textArea = new JTextArea();
 	public EmpFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("사원정보");
+		c.add(textArea);
 		setSize(300, 300);
 		setVisible(true);
-		
-		empDao.getConnection();
+		empDao.getEmpList();
+		EmpVo empVo = empDao.getEmpList();
+		int empno = empVo.getEmpno();
+		String ename = empVo.getEname();
+		String job = empVo.getJob();
+		int sal = empVo.getSal();
+		textArea.append(empno + " | ");
+		textArea.append(ename + " | ");
+		textArea.append(job + " | ");
+		textArea.append(sal + " | ");
+		//empDao.getConnection();
 	}
 	public static void main(String[] args) {
 		new EmpFrame();
