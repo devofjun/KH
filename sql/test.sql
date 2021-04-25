c55459dc7895483e804f825cd706920972770d46
zzzless <beng0516@naver.com>
2021-04-26 00:34:39 +0900

    sql test

--- sql/test.sql
+++ sql/test.sql
@@ -1,1 +1,16 @@
 select * from dual;
+
+-- create
+create table tbl_test(
+    usrid varchar2(15),
+    score number
+);
+
+-- insert
+insert into tbl_user (u_id, u_pw, u_name)
+values ('test', '1234', 'test');
+
+-- delete
+
+
+-- select
