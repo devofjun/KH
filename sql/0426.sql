32b59b10c944dd5ccd1fe910ad7cefe6a62d3d27
zzzless <beng0516@naver.com>
2021-04-26 11:23:39 +0900

    컬럼레벨 제약조건

--- sql/0426.sql
+++ sql/0426.sql
@@ -97,8 +97,85 @@ insert into emp05(empno, ename, job, deptno)
 values (1001, '박사랑', '대리', 20); -- unique 제약조건에 위배됨
 
 
+-- foreign key 사용해보기
+-- 부서테이블(dept)의 부서번호(deptno) 컬럼을 참조
+-- 해당컬럼에 값을 넣을때 참조되는 테이블에 존재하는 값만 넣을 수 있다.
+
+create table emp06(
+    empno number(4) constraint pk_emp06_empno primary key,
+    ename varchar2(10) constraint nn_emp06_ename not null,
+    job varchar2(10),
+    deptno number(2) 
+);
+
+insert into emp06(empno, ename, job, deptno)
+values (1001, '김사랑', '사원', 10);
+
+-- 부서테이블에 없는 부서인 60번 부서로 배정
+insert into emp06(empno, ename, job, deptno)
+values (1001, '박사랑', '대리', 60); -- 60번 부서 허용하지 않음
+
+
+-- check 제약조건 사용해보기
+-- check 제약 조건은 해당 컬럼의 값을 특정 값이나 범위로 한정
+-- 사번(pk), 이름(nn), 급여(500이상 5000이하), 성별(M, F)
+
+create table emp07(
+    empno number(4) constraint pk_emp07_empno primary key,
+    ename varchar2(10) constraint nn_emp07_ename not null,
+    sal number(7,2) constraint ck_emp07_sal check(sal between 500 and 5000),
+    gender varchar2(1) constraint ck_emp07_gender check(gender in ('M', 'F'))
+);
+
+-- 급여가 체크 범위를 벗어나는 데이터 입력
+-- check 제약조건 위배
+insert into emp07(empno, ename, sal, gender)
+values (1001, '김사랑', 6000, 'F');
+
+-- 성별(gender)에 설정되지 않은 M, F 이외의 값을 입력
+-- check 제약조건 위배
+insert into emp07(empno, ename, sal, gender)
+values(1001, '김사랑', 1000, 'A'); -- 'A' 는 gender에 허용된 값이 아님
+
+-- default 제약조건 -> 생략되어있다면 기본값이 입력되도록한다.
+-- 보통 입사일 같은 속성에 현재시간이 기본값으로 들어가게 만들때 많이 사용된다.
+-- 해당컬럼에 기본값(default) 설정
+-- 부서테이블에 loc(위치, 지역)에 '서울'이라는 값을 기본값으로 설정
+create table dept01(
+    deptno number(2),
+    dname varchar(10),
+    loc varchar2(10) default '서울'
+);
+
+-- loc에 값 제외하고 데이터 추가
+insert into dept01(deptno, dname)
+values (10, '경리부');
+
+select * from dept01;
 
 
 select *
 from user_constraints;
-select * from emp05;
+
+select * from emp07;
+
+
+-- number puzzle 에 사용된 테이블 삭제 후 제약조건 설정
+drop table tbl_score;
+drop table tbl_user;
+
+create table tbl_user(
+    u_id varchar2(20)
+        constraint pk_user_id primary key,
+    u_pw varchar2(20)
+        constraint nn_user_pw not null,
+    u_name varchar2(20)
+        default '아무개'
+);
+
+create table tbl_score(
+    u_id varchar2(20) constraint fk_score_id references tbl_user(u_id),
+    score number constraint nn_score_socre not null
+);
+
+select * from tbl_user;
