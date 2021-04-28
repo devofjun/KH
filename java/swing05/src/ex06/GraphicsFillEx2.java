7c0d9216877f4cc1be41536da949042404709aa9
zzzless <beng0516@naver.com>
2021-04-28 16:25:14 +0900

    여러호를 가운데 그리기

---
+++ java/swing05/src/ex06/GraphicsFillEx2.java
@@ -0,0 +1,43 @@
+package ex06;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class GraphicsFillEx2 extends JFrame{
+	Container c = getContentPane();
+	MyPanel pnl = new MyPanel();
+	
+	public GraphicsFillEx2() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("여러 호를 가운데 그리기");
+		setSize(500,500);
+		setVisible(true);
+		
+		c.add(pnl);
+	}
+	
+	class MyPanel extends JPanel{
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			int centerX = this.getWidth() / 2 - 100;
+			int centerY = this.getHeight() / 2 - 100;
+			g.setColor(Color.RED);
+			g.fillArc(centerX, centerY, 200, 200, 90, 120);
+			g.setColor(Color.BLUE);
+			g.fillArc(centerX, centerY, 200, 200, 210, 120);
+			g.setColor(Color.YELLOW);
+			g.fillArc(centerX, centerY, 200, 200, 330, 120);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		new GraphicsFillEx2();
+	}
+}
