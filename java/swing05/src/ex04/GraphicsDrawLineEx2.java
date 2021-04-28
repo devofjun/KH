418139a119c3d3cd15c4a1fb0efc7274ab50687b
zzzless <beng0516@naver.com>
2021-04-28 12:19:02 +0900

    창크기에 따라 색과크기가 균등하게 변하는 마름모

--- java/swing05/src/ex04/GraphicsDrawLineEx2.java
+++ java/swing05/src/ex04/GraphicsDrawLineEx2.java
@@ -15,7 +15,7 @@ public class GraphicsDrawLineEx2 extends JFrame{
 	
 	public GraphicsDrawLineEx2() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("마름모");
+		setTitle("마름모 그리기");
 		setSize(500,500);
 		setVisible(true);
 		
@@ -28,24 +28,24 @@ public class GraphicsDrawLineEx2 extends JFrame{
 			super.paintComponent(g);
 			//System.out.println(this.getWidth());
 			//System.out.println(this.getHeight());
-			int xHalf = this.getWidth()/2;
-			int xFull = this.getWidth();
-			int yHalf = this.getHeight()/2;
-			int yFull = this.getHeight();
+			int halfX = this.getWidth()/2;
+			int fullX = this.getWidth();
+			int halfY = this.getHeight()/2;
+			int fullY = this.getHeight();
 			
-			int layer = 0;
 			for(int i=0; i<10; i++) {
 				int red = (int)(Math.random()*256); // 색은 0~255까지
 				int green = (int)(Math.random()*256);
 				int blue = (int)(Math.random()*256);
 				g.setColor(new Color(red,green,blue));
 				
-				layer = (i*25);
-				//layer = (this.getWidth()/10)*i;
-				g.drawLine(xHalf, 0+layer, xFull-layer, yHalf); // 12-3
-				g.drawLine(xFull-layer, yHalf, xHalf, yFull-layer); // 3-6
-				g.drawLine(xHalf, yFull-layer, 0+layer, yHalf); // 6-9
-				g.drawLine(0+layer, yHalf, xHalf, 0+layer); // 9-12
+				//layer = (i*25);
+				int wLayer = ((this.getWidth()/2)/10)*i;
+				int hLayre = ((this.getHeight()/2)/10)*i;
+				g.drawLine(halfX, 			0+hLayre, 		fullX-wLayer, 	halfY); // 12-3
+				g.drawLine(fullX-wLayer, 	halfY, 			halfX, 			fullY-hLayre); // 3-6
+				g.drawLine(halfX, 			fullY-hLayre, 	0+wLayer, 		halfY); // 6-9
+				g.drawLine(0+wLayer, 		halfY, 			halfX, 			0+hLayre); // 9-12
 			}
 			
 			
