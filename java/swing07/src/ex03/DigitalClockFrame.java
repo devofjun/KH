05eed987ff7dcb3c5e4aa2734ef8d51ec34c8bb7
zzzless <beng0516@naver.com>
2021-04-29 17:30:49 +0900

    Refactoring:실시간 한국시간과 뉴욕시간

--- java/swing07/src/ex03/DigitalClockFrame.java
+++ java/swing07/src/ex03/DigitalClockFrame.java
@@ -12,35 +12,37 @@ import javax.swing.SwingConstants;
 @SuppressWarnings("serial")
 public class DigitalClockFrame extends JFrame{
 	Container c = getContentPane();
+	/*
 	JLabel korT = new JLabel("시간");
 	JLabel seoul = new JLabel("서울");
 	JLabel newT = new JLabel("시간");
 	JLabel newyork = new JLabel("뉴욕");
-	
-	
+	*/
+	private static final int SEOUL = 0;
+	private static final int NEWYORK = 1;
+	private static final int SEOULTIME = 2;
+	private static final int NEWYORKTIME = 3;
+	JLabel[] lbl = {
+			new JLabel("서울"),
+			new JLabel("뉴욕"),
+			new JLabel("서울시간"),
+			new JLabel("뉴욕시간")
+	};
 	
 	public DigitalClockFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("시계");
 		setSize(400, 400);
 		setVisible(true);
-		
-		korT.setFont(new Font("맑은 고딕", Font.BOLD, 30));
-		korT.setHorizontalAlignment(SwingConstants.CENTER);
-		seoul.setFont(new Font("맑은 고딕", Font.BOLD, 30));
-		seoul.setHorizontalAlignment(SwingConstants.CENTER);
-		newT.setFont(new Font("맑은 고딕", Font.BOLD, 30));
-		newT.setHorizontalAlignment(SwingConstants.CENTER);
-		newyork.setFont(new Font("맑은 고딕", Font.BOLD, 30));
-		newyork.setHorizontalAlignment(SwingConstants.CENTER);
 		c.setLayout(new GridLayout(2, 0));
-		c.add(seoul);
-		c.add(newyork);
-		c.add(korT);
-		c.add(newT);
+		for(int i=0; i<lbl.length; i++) {
+			lbl[i].setFont(new Font("맑은 고딕", Font.BOLD, 30));
+			lbl[i].setHorizontalAlignment(SwingConstants.CENTER);
+			c.add(lbl[i]);
+		}
 		
-		TimerThread timer = new TimerThread(korT, 0);
-		TimerThread timer2 = new TimerThread(newT, -13);
+		TimerThread timer = new TimerThread(lbl[SEOULTIME], 0);
+		TimerThread timer2 = new TimerThread(lbl[NEWYORKTIME], -13);
 		
 		timer.start();
 		timer2.start();
