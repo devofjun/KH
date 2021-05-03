b2c308318391c402138442048bcc86faca386dfb
zzzless <beng0516@naver.com>
2021-05-03 16:39:26 +0900

    파일 입력 스트림

---
+++ java/swing08/src/ex01/FileInputStreamEx.java
@@ -0,0 +1,23 @@
+package ex01;
+
+
+import java.io.FileReader;
+
+public class FileInputStreamEx {
+	public static void main(String[] args) {
+		String filePath = "G:/stream/test.txt";
+		try {
+			FileReader reader = new FileReader(filePath); // 빨대를 생성한다.
+			while(true) {
+				int data = reader.read(); // 빨대로부터 데이터를 읽어온다.
+				if(data == -1) { // 파일의 끝(-1)
+					break;
+				}
+				System.out.print((char)data); // 읽어온 데이터 출력
+			}
+			reader.close(); // 빨대를 정리한다.
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ java/swing08/src/ex01/re_FileInputStreamEx.java
@@ -0,0 +1,24 @@
+package ex01;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class re_FileInputStreamEx {
+	public static void main(String[] args) {
+		String path = "G:/stream/test.txt";
+		try {
+			FileReader reader = new FileReader(path); // 메모리에 path파일을 연다.
+			while(true) {
+				int readData = reader.read(); // 메모리의 데이터를 한글자 읽어온다.
+				if(readData == -1) { // 파일의 끝이라면 반복문 종료
+					break;
+				}
+				System.out.print((char)readData); // 읽어온 데이터 출력
+			}
+			reader.close(); // 열린 자원을 해제한다.
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
