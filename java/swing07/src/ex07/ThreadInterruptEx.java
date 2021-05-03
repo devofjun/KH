ada9c39d588147cbf132b1eac06fa74e951968ec
zzzless <beng0516@naver.com>
2021-05-03 11:07:09 +0900

    interrupt 발생하여 쓰레드 종료

---
+++ java/swing07/src/ex07/ThreadInterruptEx.java
@@ -0,0 +1,49 @@
+package ex07;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+@SuppressWarnings("serial")
+public class ThreadInterruptEx extends JFrame implements ActionListener{
+	Container c = getContentPane();
+	JLabel lbl = new JLabel("0");
+	JButton btn = new JButton("Kill Thread");
+	TimerRunnable timer;
+	Thread th;
+	
+	public ThreadInterruptEx() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("스레드 강제종료");
+		setSize(250,200);
+		
+		lbl.setFont(new Font("consolas", Font.BOLD, 50));
+		btn.addActionListener(this);
+		
+		timer = new TimerRunnable(lbl);
+		th = new Thread(timer);
+		th.start();
+		
+		c.setLayout(new FlowLayout());
+		c.add(lbl);
+		c.add(btn);
+		
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new ThreadInterruptEx();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// Thread를 강제종료
+		th.interrupt();
+	}
+}
---
+++ java/swing07/src/ex07/TimerRunnable.java
@@ -0,0 +1,28 @@
+package ex07;
+
+import javax.swing.JLabel;
+
+public class TimerRunnable implements Runnable{
+	JLabel lbl;
+	public TimerRunnable(JLabel lbl) {
+		this.lbl = lbl;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("test");
+		while(true) {
+			int i = Integer.parseInt(lbl.getText());
+			lbl.setText(String.valueOf(++i));
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// 예외발생
+				System.out.println("스레드 강제종료");
+				return;
+				//e.printStackTrace();
+			}			
+		}
+	}
+	
+}
