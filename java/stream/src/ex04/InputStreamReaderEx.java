d673d06604b4bf9a3c9ef240cd5b042e1d637791
zzzless <beng0516@naver.com>
2021-05-06 00:09:10 +0900

    주석변경

--- java/stream/src/ex04/InputStreamReaderEx.java
+++ java/stream/src/ex04/InputStreamReaderEx.java
@@ -13,22 +13,22 @@ public class InputStreamReaderEx {
 		
 		try {
 			// file -> FileInputStream -> InputStreamReader -> App
-			fis = new FileInputStream(filePath);
-			isr = new InputStreamReader(fis,"utf-8");
+			fis = new FileInputStream(filePath); // 데이터를 읽어올 파일 생성
+			isr = new InputStreamReader(fis,"utf-8"); // 읽어온 데이터와  문자형식(CharSet)을 선택
 			while(true) {
-				int data = isr.read();
-				if(data == -1) {
+				int data = isr.read(); // InputStreamReader로 부터 '한글자'를 읽어온다.
+				if(data == -1) { // 파일의 끝이라면 종료
 					fis.close();
 					isr.close();
 					return;
 				}
-				System.out.print((char)data);
+				System.out.print((char)data); // 출력
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
+		} finally { // 중간에 오류가 나서 catch로 넘어갔을 경우에도 자원을 해제하기 위한 finally
 			try {
 				fis.close();
 				isr.close();
