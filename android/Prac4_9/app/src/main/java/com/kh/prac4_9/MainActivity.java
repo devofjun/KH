25712f107fbd0aa42a6c467bf98e3401c3f203ce
zzzless <beng0516@naver.com>
2021-05-10 16:54:20 +0900

    버튼 누를때마다 이미지버튼 돌리기

---
+++ android/Prac4_9/app/src/main/java/com/kh/prac4_9/MainActivity.java
@@ -0,0 +1,29 @@
+package com.kh.prac4_9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1, btn2;
+    float angle = 0.0f;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                angle += 10.0f;
+
+                btn2.setRotation(angle);
+            }
+        });
+    }
+}
