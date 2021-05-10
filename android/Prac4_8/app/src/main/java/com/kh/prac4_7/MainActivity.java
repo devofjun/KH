51f55d3ed7afd6abd13571f26201d446399fe378
zzzless <beng0516@naver.com>
2021-05-10 16:51:59 +0900

    edittext에 글자를 입력할때마다 토스트메세지출력

---
+++ android/Prac4_8/app/src/main/java/com/kh/prac4_7/MainActivity.java
@@ -0,0 +1,40 @@
+package com.kh.prac4_7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+
+        text.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Editable txt = text.getText();
+
+                Toast.makeText(getApplicationContext(), txt.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
