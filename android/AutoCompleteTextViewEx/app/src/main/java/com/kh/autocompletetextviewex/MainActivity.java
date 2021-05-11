ed4d7f8eb21e2f05cd6dfa0cb3f9ca6af1010e6f
zzzless <beng0516@naver.com>
2021-05-11 16:23:10 +0900

    자동완성텍뷰, ArrayAdapter사용예제

---
+++ android/AutoCompleteTextViewEx/app/src/main/java/com/kh/autocompletetextviewex/MainActivity.java
@@ -0,0 +1,34 @@
+package com.kh.autocompletetextviewex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView auto;
+    MultiAutoCompleteTextView multi;
+    String[] items = {"CSI-뉴욕","CSI-라스베가스","CSI-마이애미","Friends","Fringe","Lost"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        auto = findViewById(R.id.auto);
+        multi = findViewById(R.id.multi);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_dropdown_item_1line,
+                items
+        );
+        auto.setAdapter(adapter);
+
+        MultiAutoCompleteTextView.CommaTokenizer token = new MultiAutoCompleteTextView.CommaTokenizer();
+        multi.setTokenizer(token);
+        multi.setAdapter(adapter);
+    }
+}
