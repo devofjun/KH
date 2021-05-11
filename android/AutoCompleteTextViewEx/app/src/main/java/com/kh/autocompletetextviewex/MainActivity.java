f5f7522fc6037075b4cfe5c0968a543929ad453c
zzzless <beng0516@naver.com>
2021-05-11 16:31:20 +0900

    자동완성텍뷰, ArrayAdapter사용예제 tokenizer구현해보기

--- android/AutoCompleteTextViewEx/app/src/main/java/com/kh/autocompletetextviewex/MainActivity.java
+++ android/AutoCompleteTextViewEx/app/src/main/java/com/kh/autocompletetextviewex/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         auto = findViewById(R.id.auto);
         multi = findViewById(R.id.multi);
 
+        // Array
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_dropdown_item_1line,
@@ -27,7 +28,9 @@ public class MainActivity extends AppCompatActivity {
         );
         auto.setAdapter(adapter);
 
+        // 토큰은 기본적으로 (,) 밖에 안되지만 새로운 클래스에서 Tokenizer 인터페이스를 구현하면 바꿀수있다.
         MultiAutoCompleteTextView.CommaTokenizer token = new MultiAutoCompleteTextView.CommaTokenizer();
+        // MultiAutoCompleteTextView는 토큰을 지정해주지 않으면 아예 동작하지 않는다.
         multi.setTokenizer(token);
         multi.setAdapter(adapter);
     }
