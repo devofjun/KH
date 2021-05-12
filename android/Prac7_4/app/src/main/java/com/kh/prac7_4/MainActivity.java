97952013eb9449c0071bd04d843e164e56ac0c49
zzzless <beng0516@naver.com>
2021-05-12 14:41:49 +0900

    옵션매뉴로 이미지조정하기_주석

--- android/Prac7_4/app/src/main/java/com/kh/prac7_4/MainActivity.java
+++ android/Prac7_4/app/src/main/java/com/kh/prac7_4/MainActivity.java
@@ -18,14 +18,16 @@ public class MainActivity extends AppCompatActivity {
         img = findViewById(R.id.img);
 
     }
-
+    // 옵션 메뉴 생성
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // 메뉴 전개자(inflater : xml의 태그를 java 객체로 변환 시킴)
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu1,menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+    // 메뉴 옵션 선택
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
