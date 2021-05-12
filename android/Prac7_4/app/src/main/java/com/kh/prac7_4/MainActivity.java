e28cbd01cb766902519c40c4fc007435a3471aad
zzzless <beng0516@naver.com>
2021-05-12 12:37:34 +0900

    옵션매뉴로 이미지조정하기

---
+++ android/Prac7_4/app/src/main/java/com/kh/prac7_4/MainActivity.java
@@ -0,0 +1,46 @@
+package com.kh.prac7_4;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img = findViewById(R.id.img);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1,menu);
+        menu.
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.mnuDog:
+                img.setImageResource(R.drawable.dog);
+                break;
+            case R.id.mnuCat:
+                img.setImageResource(R.drawable.cat);
+                break;
+            case R.id.mnuRabbit:
+                img.setImageResource(R.drawable.rabbit);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
