07f85dd0167906502a50d037511863b2f60b6228
zzzless <beng0516@naver.com>
2021-05-12 12:07:22 +0900

    메뉴만들어보기

---
+++ android/MenuEx/app/src/main/java/com/kh/menuex/MainActivity.java
@@ -0,0 +1,57 @@
+package com.kh.menuex;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout baseLayout;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("배경색 바꾸기");
+        baseLayout = findViewById(R.id.baseLayout);
+        btn = findViewById(R.id.btn);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch(id) {
+            case R.id.itemRed:
+                baseLayout.setBackgroundColor(Color.RED);
+                break;
+            case R.id.itemGreen:
+                baseLayout.setBackgroundColor(Color.GREEN);
+                break;
+            case R.id.itemBlue:
+                baseLayout.setBackgroundColor(Color.BLUE);
+                break;
+            case R.id.btnRotate:
+                btn.setRotation(btn.getRotation()+45);
+                break;
+            case R.id.btnScale:
+                btn.setScaleX(btn.getScaleX()*2);
+                btn.setScaleY(btn.getScaleY()*2);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
