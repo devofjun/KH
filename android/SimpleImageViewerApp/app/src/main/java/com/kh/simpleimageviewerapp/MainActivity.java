711aa738185be4e1cfac1963cf65e52ce9316f5e
zzzless <beng0516@naver.com>
2021-05-17 14:08:59 +0900

    [간단한 SDCard 이미지뷰]: 코드설명주석추가, 첫 이미지 출력 imgFiles로 수정

--- android/SimpleImageViewerApp/app/src/main/java/com/kh/simpleimageviewerapp/MainActivity.java
+++ android/SimpleImageViewerApp/app/src/main/java/com/kh/simpleimageviewerapp/MainActivity.java
@@ -31,20 +31,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPrev.setOnClickListener(this);
         btnNext.setOnClickListener(this);
 
+        // 읽어올 파일 경로
         File f = new File("/sdcard/Pictures");
+        // 해당 경로의 모든 파일목록
         files = f.listFiles();
         for(int i=0; i< files.length; i++) {
-            //Log.d("mytag", "list: "+files[i].toString());
+            // 파일이름
             String filename = files[i].toString();
+            // 파일 확장자 읽기
             String extension = filename.substring(filename.lastIndexOf(".")+1);
-            //Log.d("mytag", "list: "+files[i].toString()+"|ext|: " + extension);
+            // 파일 확장자가 이미지 파일일 경우만 imgFiles에 담음
             if(extension.equals("jpg") || extension.equals("png") || extension.equals("gif")){
                 imgFiles.add(files[i]);
             } else {
                // Log.d("mytag","noImgIdx:"+i);
             }
         }
-        myView.filePath = files[index].toString();
+        // 첫 화면에 나올 이미지
+        myView.filePath = imgFiles.get(index).toString();
+        // 인덱스 표시
         textView.setText((index+1)+"/"+(imgFiles.size()));
     }
 
@@ -54,18 +59,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if(index > 0) {
                 --index;
             } else {
+                // 인덱스의 경계를 넘으려하면 반대편 인덱스로 변경
                 index = imgFiles.size() -1;
             }
         } else if(v == btnNext) {
             if(index < imgFiles.size() - 1) {
                 ++index;
             } else {
+                // 인덱스의 경계를 넘으려하면 반대편 인덱스로 변경
                 index = 0;
             }
         }
-        //Log.d("mytag", "index: "+index);
+        // 인덱스를 변경한 후 그려질 이미지의 경로를 저장한다.
         myView.filePath = imgFiles.get(index).toString();
-        myView.invalidate(); // 무효화하다. -> 다시 그리기 요청
+        // 무효화하다. -> 다시 그리기 요청
+        myView.invalidate();
+        // 인덱스 최신화
         textView.setText((index+1)+"/"+(imgFiles.size()));
     }
 }
