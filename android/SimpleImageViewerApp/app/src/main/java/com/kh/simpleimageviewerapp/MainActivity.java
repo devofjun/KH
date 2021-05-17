90026f7e394b26c17ab467ed5f5c9f4d1ccd4313
zzzless <beng0516@naver.com>
2021-05-17 11:03:28 +0900

    [SD카드이미지뷰어]: 확장자 검사_이미지가 아닌index는 건너뛰기버전

--- android/SimpleImageViewerApp/app/src/main/java/com/kh/simpleimageviewerapp/MainActivity.java
+++ android/SimpleImageViewerApp/app/src/main/java/com/kh/simpleimageviewerapp/MainActivity.java
@@ -3,12 +3,102 @@ package com.kh.simpleimageviewerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnPrev, btnNext;
+    myPictureView myView;
+    File[] files;
+    int index = 0;
+    int imgLength = 0;
+    ArrayList<Integer> noImgIdx = new ArrayList<Integer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnPrev = findViewById(R.id.btnPrev);
+        btnNext = findViewById(R.id.btnNext);
+        myView = findViewById(R.id.myView);
+        // 버튼 클릭 리스너
+        btnPrev.setOnClickListener(this);
+        btnNext.setOnClickListener(this);
+
+        File f = new File("/sdcard/Pictures");
+        files = f.listFiles();
+        for(int i=0; i< files.length; i++) {
+            //Log.d("mytag", "list: "+files[i].toString());
+            String filename = files[i].toString();
+            String extension = filename.substring(filename.lastIndexOf(".")+1);
+            Log.d("mytag", "list: "+files[i].toString()+"|ext|: " + extension);
+            if(extension.equals("jpg") || extension.equals("png") || extension.equals("gif")){
+                ++imgLength;
+            } else {
+                noImgIdx.add(i);
+            }
+        }
+        myView.filePath = files[index].toString();
+        for(int i=0; i < noImgIdx.size(); i++){
+            Log.d("mytag","noImage: "+noImgIdx.get(i));
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btnPrev){
+            if(index > 0) {
+                int i = 0;
+                while(true){
+                    if(index == noImgIdx.get(i++) && noImgIdx.size() > i){
+                        if(index-2 < 0){
+                            Toast.makeText(this, "이전 그림 없음", Toast.LENGTH_SHORT).show();
+                        } else {
+                            index -= 2;
+                        }
+                        break;
+                    } else if(noImgIdx.size() <= i){
+                        if(index-1 < 0){
+                            Toast.makeText(this, "이전 그림 없음", Toast.LENGTH_SHORT).show();
+                        } else {
+                            --index;
+                        }
+                        break;
+                    }
+                }
+            } else {
+                Toast.makeText(this, "이전 그림 없음", Toast.LENGTH_SHORT).show();
+            }
+        } else if(v == btnNext) {
+            if(index < files.length - 1) {
+                int i = 0;
+                while(true){
+                    if(index == noImgIdx.get(i++) && noImgIdx.size() > i){
+                        if(files.length <= index+2){
+                            Toast.makeText(this, "다음 그림 없음", Toast.LENGTH_SHORT).show();
+                        } else {
+                            index+=2;
+                        }
+                        break;
+                    } else if(noImgIdx.size() <= i){
+                        if(files.length <= index+2){
+                            Toast.makeText(this, "다음 그림 없음", Toast.LENGTH_SHORT).show();
+                        } else {
+                            ++index;
+                        }
+                        break;
+                    }
+                }
+            } else {
+                Toast.makeText(this, "다음 그림 없음", Toast.LENGTH_SHORT).show();
+            }
+        }
+        Log.d("mytag", "index: "+index);
+        myView.filePath = files[index].toString();
+        myView.invalidate(); // 무효화하다. -> 다시 그리기 요청
     }
 }
--- android/SimpleImageViewerApp/app/src/main/java/com/kh/simpleimageviewerapp/myPictureView.java
+++ android/SimpleImageViewerApp/app/src/main/java/com/kh/simpleimageviewerapp/myPictureView.java
@@ -1,5 +1,39 @@
 package com.kh.simpleimageviewerapp;
 
-public class myPictureView {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
 
+import androidx.annotation.Nullable;
+
+public class myPictureView extends View {
+    String filePath;
+
+    // java 코드로 생성시
+    public myPictureView(Context context) {
+        super(context);
+    }
+    // XML 이용해서 생성시
+    public myPictureView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+    // 스윙의 paintComponent(Graphics g) - g: 붓
+
+    @Override
+    protected void onDraw(Canvas canvas) { // canvas : 도화지
+        super.onDraw(canvas);
+        //filePath = "/sdcard/Pictures/cat.jpg";
+        if(filePath == null){
+            return;
+        }
+        Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+        // 비트맵 이미지 그리기 - (0,0)의 위치에 그림
+        canvas.drawBitmap(bitmap,0,0,null);
+        // 이미지는 메모리를 많이 차지한다.
+        bitmap.recycle();
+    }
 }
