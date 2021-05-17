43d2a0fdbea1edbd6faa952f1c28b270c8dac87e
zzzless <beng0516@naver.com>
2021-05-17 11:21:24 +0900

    [SD카드이미지뷰어]: 리팩토링_파일 어레이리스트 활용

--- android/SimpleImageViewerApp/app/src/main/java/com/kh/simpleimageviewerapp/MainActivity.java
+++ android/SimpleImageViewerApp/app/src/main/java/com/kh/simpleimageviewerapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -16,8 +17,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     myPictureView myView;
     File[] files;
     int index = 0;
-    int imgLength = 0;
-    ArrayList<Integer> noImgIdx = new ArrayList<Integer>();
+    ArrayList<File> imgFiles = new ArrayList<>();
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPrev = findViewById(R.id.btnPrev);
         btnNext = findViewById(R.id.btnNext);
         myView = findViewById(R.id.myView);
+        textView = findViewById(R.id.textView);
         // 버튼 클릭 리스너
         btnPrev.setOnClickListener(this);
         btnNext.setOnClickListener(this);
@@ -37,68 +39,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String extension = filename.substring(filename.lastIndexOf(".")+1);
             Log.d("mytag", "list: "+files[i].toString()+"|ext|: " + extension);
             if(extension.equals("jpg") || extension.equals("png") || extension.equals("gif")){
-                ++imgLength;
+                imgFiles.add(files[i]);
             } else {
-                noImgIdx.add(i);
+                Log.d("mytag","noImgIdx:"+i);
             }
         }
         myView.filePath = files[index].toString();
-        for(int i=0; i < noImgIdx.size(); i++){
-            Log.d("mytag","noImage: "+noImgIdx.get(i));
-        }
+        textView.setText((index+1)+"/"+(imgFiles.size()));
     }
 
     @Override
     public void onClick(View v) {
         if(v == btnPrev){
             if(index > 0) {
-                int i = 0;
-                while(true){
-                    if(index == noImgIdx.get(i++) && noImgIdx.size() > i){
-                        if(index-2 < 0){
-                            Toast.makeText(this, "이전 그림 없음", Toast.LENGTH_SHORT).show();
-                        } else {
-                            index -= 2;
-                        }
-                        break;
-                    } else if(noImgIdx.size() <= i){
-                        if(index-1 < 0){
-                            Toast.makeText(this, "이전 그림 없음", Toast.LENGTH_SHORT).show();
-                        } else {
-                            --index;
-                        }
-                        break;
-                    }
-                }
+                --index;
             } else {
-                Toast.makeText(this, "이전 그림 없음", Toast.LENGTH_SHORT).show();
+                index = imgFiles.size() -1;
             }
         } else if(v == btnNext) {
-            if(index < files.length - 1) {
-                int i = 0;
-                while(true){
-                    if(index == noImgIdx.get(i++) && noImgIdx.size() > i){
-                        if(files.length <= index+2){
-                            Toast.makeText(this, "다음 그림 없음", Toast.LENGTH_SHORT).show();
-                        } else {
-                            index+=2;
-                        }
-                        break;
-                    } else if(noImgIdx.size() <= i){
-                        if(files.length <= index+2){
-                            Toast.makeText(this, "다음 그림 없음", Toast.LENGTH_SHORT).show();
-                        } else {
-                            ++index;
-                        }
-                        break;
-                    }
-                }
+            if(index < imgFiles.size() - 1) {
+                ++index;
             } else {
-                Toast.makeText(this, "다음 그림 없음", Toast.LENGTH_SHORT).show();
+                index = 0;
             }
         }
         Log.d("mytag", "index: "+index);
-        myView.filePath = files[index].toString();
+        myView.filePath = imgFiles.get(index).toString();
         myView.invalidate(); // 무효화하다. -> 다시 그리기 요청
+        textView.setText((index+1)+"/"+(imgFiles.size()));
     }
 }
