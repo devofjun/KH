7869d8323f657bf25c1c562e515e1800ae98dc5b
zzzless <beng0516@naver.com>
2021-05-23 20:32:21 +0900

    [옵션메뉴]: 주석추가

--- android/MenuEx/app/src/main/java/com/kh/menuex/MainActivity.java
+++ android/MenuEx/app/src/main/java/com/kh/menuex/MainActivity.java
@@ -18,21 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 이 activity의 제목 설정
         setTitle("배경색 바꾸기");
         baseLayout = findViewById(R.id.baseLayout);
         btn = findViewById(R.id.btn);
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // 메뉴전개자 가져옴
         MenuInflater inflater = getMenuInflater();
+        // (직접만든 메뉴, 타겟이 될 엑티비티의 메뉴);
         inflater.inflate(R.menu.menu1,menu);
+        // 직접만든 메뉴를 타겟 메뉴에 그렸으니 super에 정의된 메소드를 이용해서 메뉴를 생성한다.
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // 선택된 메뉴 아이템의 아이디
         int id = item.getItemId();
         switch(id) {
             case R.id.itemRed:
@@ -52,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
                 btn.setScaleY(btn.getScaleY()*2);
                 break;
         }
+        // 나머지 동작(메뉴닫기?)은 그대로 실행하기위해 super의 메소드를 호출한다.
         return super.onOptionsItemSelected(item);
     }
 }
