baf38f8b08907383c005d40e8b5eb587e912c750
zzzless <beng0516@naver.com>
2021-06-05 13:08:55 +0900

    test

--- sql/jspUser.sql
+++ sql/jspUser.sql
@@ -4,3 +4,8 @@ grant connect, resource to jsp01;
 create user jsp02 identified by 1234;
 grant connect, resource to jsp02;
 
+create user test01 identified by 1234;
+grant connect, resource to test01;
+
+create user EXAM01 identified by 1234;
+grant connect, resource to EXAM01;
