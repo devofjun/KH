c50be8c89f0c2c5e3791a402cf9a988993e0d1ec
zzzless <beng0516@naver.com>
2021-06-15 13:47:59 +0900

    답글 주석

--- Model2/Model2/src/com/kh/service/BoardReplyFormService.java
+++ Model2/Model2/src/com/kh/service/BoardReplyFormService.java
@@ -9,10 +9,11 @@ public class BoardReplyFormService implements IService {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// 답글을 달기 위해 필요한 정보
 		int re_group = Integer.parseInt(request.getParameter("re_group"));
 		int re_seq = Integer.parseInt(request.getParameter("re_seq"));
 		int re_level = Integer.parseInt(request.getParameter("re_level"));
-		
+		// 필요한 정보를 request로 다시 담아서 보내준다.
 		BoardVo boardVo = new BoardVo();
 		boardVo.setRe_group(re_group);
 		boardVo.setRe_seq(re_seq);
