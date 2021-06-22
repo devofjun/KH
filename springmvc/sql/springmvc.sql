3474831a0f7ea5463ff3028552e3cf1c2f72d1b7
zzzless <beng0516@naver.com>
2021-06-22 12:22:33 +0900

    유저정보 생성

---
+++ springmvc/sql/springmvc.sql
@@ -0,0 +1,13 @@
+create user springmvc identified by 1234;
+grant connect, resource to springmvc;
+
+-- springmvc 접속
+-- 회원 테이블
+create table tbl_member(
+    user_id varchar2(50) primary key,
+    user_pw varchar2(50) not null,
+    user_name varchar2(50) not null,
+    user_email varchar2(50),
+    reg_date timestamp default sysdate,
+    update_date timestamp
+);
