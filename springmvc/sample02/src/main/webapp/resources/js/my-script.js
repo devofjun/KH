1babc83ef3ef3f4e975cac6f69085abc6f730cc2
zzzless <beng0516@naver.com>
2021-06-28 12:25:57 +0900

    날짜 포멧
    날짜 포멧 여러 방법 제시

--- springmvc/sample02/src/main/webapp/resources/js/my-script.js
+++ springmvc/sample02/src/main/webapp/resources/js/my-script.js
@@ -2,13 +2,21 @@
  * my-script.js
  */
 
+function make2digits(num){
+	if(num<10){
+		num = "0" + num;
+	}
+	return num;
+}
+
 function changeDateString(timestamp){
 	var d = new Date(timestamp);
 	var year = d.getFullYear();
-	var month = d.getMonth() + 1;
-	var date = d.getDate();
-	var hour = d.getHours();
-	var minute = d.getMinutes();
-	var second = d.getSeconds();
-	return year + "-" + month + "-" + date  + "-" +  hour  + "-" +  minute  + "-" +  second; 
+	var month = make2digits(d.getMonth() + 1);
+	var date = make2digits(d.getDate());
+	var hour = make2digits(d.getHours());
+	var minute = make2digits(d.getMinutes());
+	var second = make2digits(d.getSeconds());
+	return year + "-" + month + "-" + date  + "-" +  hour  + "-" +  minute  + "-" +  second;
+	//return d.toLocaleString();
 }
